d9341ece96997507f28a05ab5d3481a9120f7a52
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-07-16 14:04:22 +0300

    Remove mobile zoom setting

--- pages/_app.js
+++ pages/_app.js
@@ -15,10 +15,6 @@ export default class MyApp extends App {
             href="https://fonts.googleapis.com/css2?family=Bangers&family=Galada&display=swap"
             rel="stylesheet"
           />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
         </Head>
 
         <ThemeProvider theme={theme}>
