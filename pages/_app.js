245531a49ee49dcf8e31e37bc8db91a999449825
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-07-16 13:08:45 +0300

    Update zoom on mobile device settings

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default class MyApp extends App {
           />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+            content="width=device-width, user-scalable=no"
           />
         </Head>
 
